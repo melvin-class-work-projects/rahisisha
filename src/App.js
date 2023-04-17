bcd1196d43918882edaaf236c0c9fc4f9a6cd37a
max101g <maxgithinji101@gmail.com>
2023-04-17 12:19:41 +0300

    Profile page skeleton done

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import Signup from './Components/Signup'
 import Login from './Components/Login'
 import LandingPage from './Components/LandingPage'
 import HomePage from './Components/HomePage';
+import Profile from './Components/Profile';
 
 function App() {
   return (
     <div className="App">
-    <Navbar/>
+    {/* <Navbar/> */}
     <Routes>
     <Route path="/" element={<LandingPage />} />
     <Route path="/signup" element={<Signup />} />
     <Route path="/login" element={<Login />} />
+    <Route path="/profile" element={<Profile/>} />
     <Route path='/home' element={<HomePage/>}/>
     </Routes>
     </div>
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,8 +1,102 @@
 import React from 'react'
+import './profile.css'
+import { TbEdit } from 'react-icons/tb'
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <>
+    <section className="profile__page">
+      <div className="profile__page-container">
+        <article className="profile__page-content">
+          <div className="profile__page-profile">
+            <div className="profile__page-background">
+
+            </div>
+            <div className="profile__page-avatar">
+              <img src="https://images.pexels.com/photos/5386149/pexels-photo-5386149.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            </div>
+          </div>
+          <div className="profile__card-body">
+            <div className="profile__card-about">
+              <h4>John Doe</h4>
+              <span>Software Engineer</span>
+              <div className="profile__about">
+                <span>Nairobi, Kenya</span>
+                <button>Contact info</button>
+              </div>
+            </div>
+          </div>
+        </article>
+        <div className="profle__page-edit">
+          <article className="edit__work">
+            <div className="edit__header">
+              <h4>Work</h4>
+              <TbEdit/>
+            </div>
+            <div className="edit__content">
+              <span>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam quidem porro reprehenderit iure, corporis qui.
+              </span>
+            </div>
+          </article>
+          <article className="edit__work">
+            <div className="edit__header">
+              <h4>Work</h4>
+              <TbEdit/>
+            </div>
+            <div className="edit__content">
+              <span>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam quidem porro reprehenderit iure, corporis qui.
+              </span>
+            </div>
+          </article>
+          <article className="edit__work">
+            <div className="edit__header">
+              <h4>Work</h4>
+              <TbEdit/>
+            </div>
+            <div className="edit__content">
+              <span>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam quidem porro reprehenderit iure, corporis qui.
+              </span>
+            </div>
+          </article>
+        </div>
+        <div className="profile__page-activity">
+          <div className="profile__page-active">
+            <article className="activity__card">
+              <div className="activity__card-comment">
+                <span>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, cupiditate?
+                </span>
+              </div>
+              <div className="activity__card-image">
+                <img src="https://images.pexels.com/photos/574071/pexels-photo-574071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+              </div>
+              <div className="activity__card-buttons">
+                <button className='edit'>Edit</button>
+                <button className='delete'>Delete</button>
+              </div>
+            </article>
+            <article className="activity__card">
+              <div className="activity__card-comment">
+                <span>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, cupiditate?
+                </span>
+              </div>
+              <div className="activity__card-image">
+                <img src="https://images.pexels.com/photos/574071/pexels-photo-574071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+              </div>
+              <div className="activity__card-buttons">
+                <button className='edit'>Edit</button>
+                <button className='delete'>Delete</button>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
