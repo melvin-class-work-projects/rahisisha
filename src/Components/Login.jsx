af2a06a59962ba6c169c629267172a02ff7847b9
+ <+>
2023-04-17 10:36:07 +0300

    Create Login and Signup

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import Footer from './Footer';
+
 
 function Login() {
   return (
-    <div>Login</div>
+    <>
+    <div style={{ color: "white", height: "100vh"}}>
+  <div className="overlay">
+    <div className="login-form-container border rounded p-3 " style={{width:"45vw",height:"60vh",backgroundColor:"black", marginLeft:"300px"}}>
+      <div className="login-form-overlay">
+        <div className="login-form-container">
+          <div className="login-form-header">
+            <h2>Log in</h2>
+          </div>
+  
+          <div className="login-form-body">
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">Email:</label>
+              <input type="email" className="form-control" id="email" />
+            </div>
+  
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">Password:</label>
+              <input type="password" className="form-control" id="password" />
+            </div>
+  
+            <button className="btn btn-primary">Log in</button>
+  
+            <div className="signup-link-container mt-3">
+              <p>Don't have an account?</p>
+              <button className="btn btn-outline-primary">Sign up</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<Footer/>
+</>
+
   )
 }
 
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,8 +1,76 @@
 import React from 'react'
-
+import Footer from './Footer';
+import './signup.css'
 function Signup() {
   return (
-    <div>Signup</div>
+    <>
+    <div className="signup-section">
+    
+    <form className="signup-form">
+      <h3 style={{color:'white'}}>Sign Up</h3>
+      <div className="form-group">
+        <label style={{color:'white'}} htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="username"
+          // value={formData.username}
+          // onChange={handleChange}
+        />
+      </div>
+  
+      <div className="form-group">
+        <label style={{color:'white'}} htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          // value={formData.email}
+          // onChange={handleChange}
+        />
+      </div>
+  
+      <div className="form-group">
+        <label style={{color:'white'}} htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          // value={formData.password}
+          // onChange={handleChange}
+        />
+      </div>
+  
+      <div className="form-group">
+        <label style={{color:'white'}} htmlFor="password-confirm">Confirm password:</label>
+        <input
+          type="password"
+          id="password-confirm"
+          name="password-confirm"
+          // value={formData.passwordConfirm}
+          // onChange={handleChange}
+        />
+      </div>
+  
+      <div className="form-group">
+        <label style={{color:'white'}} htmlFor="category">Category:</label>
+        <select id="category" name="category">
+          {/* <option value="admin">Admin</option> */}
+          <option value="user">Job Seeker</option>
+          <option value="employer">Employer</option>
+        </select>
+      </div>
+  
+      <button type="submit">Sign up</button>
+      <p style={{color:'white'}}>
+
+        Already have an account?{" "}
+        <a href="#">Log in</a>
+      </p>
+    </form>
+  </div>
+  <Footer/>
+  </>
   )
 }
 
