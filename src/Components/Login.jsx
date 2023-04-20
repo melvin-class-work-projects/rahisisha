61ced39cb6909ac2ff24b1ec41fc2219c0b6765f
+ <+>
2023-04-20 13:30:41 +0300

    unstaged changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,47 +1,145 @@
-import React from 'react'
-import './login.css'
-import Footer from './Footer';
-
+import React, { useState, useEffect } from "react";
+import Footer from "./Footer";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [accessToken, setAccessToken] = useState(null);
+
+  useEffect(() => {
+    const storedAccessToken = localStorage.getItem("accessToken");
+    if (storedAccessToken && storedAccessToken !== "undefined") {
+      setAccessToken(storedAccessToken);
+    }
+  }, []);
+
+  const handleLogin = () => {
+    try {
+      const decodedToken = JSON.parse(atob(accessToken.split(".")[1]));
+      const userRole = decodedToken.role;
+      localStorage.setItem("userRole", userRole);
+      if (userRole === "SEEKER") {
+        window.location.href = "/home";
+      } else if (userRole === "EMPLOYER") {
+        window.location.href = "/home";
+      } else if (userRole === "ADMIN") {
+        window.location.href = "/admin"
+      }
+    } catch (error) {
+      setErrors([error.message]);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://127.0.0.1:3000/generate-token/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        const accessToken = data.accessToken;
+
+        console.log(data)
+        localStorage.setItem("accessToken", accessToken);
+        setAccessToken(accessToken);
+      } else {
+        const data = await response.json();
+        setErrors(data.errors);
+      }
+    } catch (error) {
+      setErrors([error.message]);
+    }
+  };
+
+  const handleSignupClick = () => {
+    window.location.href = "/signup";
+  };
+
+  useEffect(() => {
+    if (accessToken) {
+      handleLogin();
+    }
+  }, [accessToken]);
+
+
   return (
     <>
-      <div style={{ color: "white", height: "100vh"}}>
-        <div className="overlay">
-          <div className="login-form-container border rounded p-3 " style={{width:"45vw",height:"60vh",backgroundColor:"black", marginLeft:"300px"}}>
-            <div className="login-form-overlay">
-              <div className="login-form-container">
+      <div
+        style={{ color: "white", minHeight: "100vh" }}
+        className="d-flex align-items-center"
+      >
+        {/* Render login form */}
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-6">
+              <div
+                className="login-form-container border rounded p-3"
+                style={{ backgroundColor: "black" }}
+              >
                 <div className="login-form-header">
                   <h2>Log in</h2>
                 </div>
-        
+
                 <div className="login-form-body">
-                  <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email:</label>
-                    <input type="email" className="form-control" id="email" />
-                  </div>
-        
-                  <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password:</label>
-                    <input type="password" className="form-control" id="password" />
-                  </div>
-        
-                  <button className="btn btn-primary">Log in</button>
-        
-                  <div className="signup-link-container mt-3">
-                    <p>Don't have an account?</p>
-                    <button className="btn btn-outline-primary">Sign up</button>
-                  </div>
+                  <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                      <label htmlFor="username" className="form-label">
+                        Username:
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                      />
+                    </div>
+
+                    <div className="mb-3">
+                      <label htmlFor="password" className="form-label">
+                        Password:
+                      </label>
+                      <input
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </div>
+
+                    <button type="submit" className="btn btn-primary">
+                      Log in
+                    </button>
+
+                    <div className="signup-link-container mt-3">
+                      <p>Don't have an account?</p>
+                      <button
+                        type="button"
+                        className="btn btn-outline-primary"
+                        onClick={handleSignupClick}
+                      >
+                        Sign up
+                      </button>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
           </div>
+          {/* ... */}
         </div>
       </div>
-<Footer/>
-</>
-
-  )
+      <Footer />
+    </>
+  );
 }
 
 export default Login;
