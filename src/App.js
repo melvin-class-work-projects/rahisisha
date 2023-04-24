643912108ff1d1e9078ec9dbf887b850dc52e6c8
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2023-04-24 13:35:21 +0300

    adding UI to the home component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import {   Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Signup from './Components/Signup';
